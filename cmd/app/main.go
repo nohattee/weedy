a24bbae73cc8e62618c6902aff30f6290cc2a50a
The Breaker <nohattee@gmail.com>
2022-05-26 18:21:25 +0700

    apply command package

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,54 @@
 package main
 
 import (
-	http "weedy/internal/controller/http/v1"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	ctrl_v1 "weedy/internal/controller/http/v1"
+	"weedy/internal/repository"
+	"weedy/pkg/config"
 	"weedy/pkg/httpserver"
+
+	_ "github.com/lib/pq"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	server := httpserver.NewHttpServer("", "8080", "")
+var rootCmd = &cobra.Command{
+	Use:   "app",
+	Short: "App is a very fast static site generator",
+	Long: `A Fast and Flexible Static Site Generator built with
+				  love by spf13 and friends in Go.
+				  Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.LoadConfigFromEnv()
 
-	server.AddController(&http.UserController{})
-	server.Run()
+		var dns string
+		if cfg.DB.Connection != "" {
+			dns = cfg.DB.Connection
+		} else {
+			dns = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", cfg.DB.Host, cfg.DB.Port, cfg.DB.Username, cfg.DB.Password, cfg.DB.Name)
+		}
+		db, err := sql.Open("postgres", dns)
+		if err != nil {
+			log.Fatalf("sql.Open err: %v", err)
+		}
+		err = db.Ping()
+		if err != nil {
+			log.Fatalf("db.Ping err: %v", err)
+		}
+
+		userRepo := &repository.UserRepo{DB: db}
+		server := httpserver.NewHttpServer(cfg.HTTP)
+		server.AddController(ctrl_v1.NewUserController(userRepo))
+		server.Run()
+	},
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,9 @@ require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-chi/render v1.0.1
 	github.com/go-playground/validator/v10 v10.11.0
+	github.com/lib/pq v1.10.6
+	github.com/rs/cors v1.8.2
+	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.7.0
 )
 
@@ -14,9 +17,10 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rs/cors v1.8.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
